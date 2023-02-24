265cf74d68227fd928f0672dbe5efcabdb5484f6
Dennis Olmedo (RIT Student) <dao9631@rit.edu>
2023-02-24 16:19:38 -0500

    Setup for Project 1

---
+++ src/jsonResponses.js
@@ -0,0 +1,9 @@
+const respondJSON = (request, response, status, object) => {
+    response.writeHead(status, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(object));
+    response.end();
+  };
+
+module.exports = {
+    
+}
